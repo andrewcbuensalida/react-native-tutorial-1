a65cdbc460a73bdd776fe6982df1f7cd2e17c6e2
andrewlove219 <andrewlove219@gmail.com>
2021-11-16 22:10:39 -0800

    working font

--- gamezone/App.js
+++ gamezone/App.js
@@ -1,27 +1,16 @@
-import React, { useState } from 'react';
-import Home from './screens/home';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
-
-const getFonts = () => Font.loadAsync({
-  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
-  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
-});
+import React from "react";
+import Home from "./screens/home";
+import { useFonts } from "expo-font";
 
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
+	const [fontsLoaded] = useFonts({
+		"nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+		"nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+	});
 
-  if (fontsLoaded) {
-    return (
-      <Home />
-    );
-  } else {
-    return (
-      <AppLoading 
-        startAsync={getFonts} 
-        onFinish={() => setFontsLoaded(true)} 
-      />
-    )
-  }
-
+	if (fontsLoaded) {
+		return <Home />;
+	} else {
+		return null;
+	}
 }
