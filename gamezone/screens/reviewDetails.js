3e3688ae8fff1513b87266ab3108a8c0f511ee16
andrewlove219 <andrewlove219@gmail.com>
2021-11-17 21:48:58 -0800

    dynamically rendering ratings

--- gamezone/screens/reviewDetails.js
+++ gamezone/screens/reviewDetails.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
 import { globalStyles } from "../styles/global";
 import Card from "../shared/card";
 
-export default function ReviewDetails({ route, navigation }) {
+export default function ReviewDetails({ route }) {
 	return (
 		<View style={globalStyles.container}>
 			<Card>
 				<Text style={globalStyles.titleText}>{route.params.title}</Text>
 				<Text style={globalStyles.paragraph}>{route.params.body}</Text>
 				<Text style={globalStyles.paragraph}>
-					Rating: {route.params.rating}
+					Rating:
+					{Array(route.params.rating)
+						.fill()
+						.map((heart) => (
+							<Image source={require("../assets/rating-1.png")} />
+						))}
 				</Text>
 			</Card>
 		</View>
